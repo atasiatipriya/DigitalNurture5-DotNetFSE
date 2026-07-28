f0e3f9dfa3442f59c33c3638308797c007634ebe
atasiatipriya <atasiatipriyarout@gmail.com>
2026-07-28 23:09:09 +0530

    Week 5: ASP.NET Core Web API - Employee CRUD with EF Core and Swagger

---
+++ week-5/EmployeeAPI/EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Department { get; set; } = string.Empty;
+
+        [Range(0, double.MaxValue)]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
